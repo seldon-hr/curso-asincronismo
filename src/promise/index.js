59b5f8d31beff30e0cab7f2b838ce2c1b67c6f50
Daniel Romero <daanmaro99june@gmail.com>
2022-08-01 16:41:57 -0500

    Uso de Promesas

--- src/promise/index.js
+++ src/promise/index.js
@@ -2,7 +2,9 @@ const promise = new Promise(function (resolve, reject) {
     resolve('hey!')
 });
 
-const cows = 7;
+
+// Una promesa que se basa en el número de vacas, y si tenemos un número podemos proceder.
+const cows = 15;
 
 const countCows = new Promise (function(resolve, reject) {
     if(cows > 10) {
@@ -12,8 +14,10 @@ const countCows = new Promise (function(resolve, reject) {
     }
 });
 
-countCows.then((result) => {
+
+//Ejecución de la función.
+countCows.then((result) => {  //Uso de 'then' con el cual se ejecuta la promesa. then para pedir
     console.log(result)
-}).catch((error) => {
+}).catch((error) => { // 'catch' para indicar que no se ha cumplido la promesa
     console.log(error);
-}).finally(() => console.log('Finally'))
+}).finally(() => console.log('Finally')) //'finally' es opcional pero es para poder añadir un mensaje que ya termino la promesa whaterever the result.
