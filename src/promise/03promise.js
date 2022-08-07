6b02ddfac1e849848492449e4db386ccc8525cff
Daniel Romero <daanmaro99june@gmail.com>
2022-08-06 22:37:55 -0500

    Promise and then, catch

--- src/promise/03promise.js
+++ src/promise/03promise.js
@@ -7,7 +7,7 @@
 
 const myPromise = new Promise((resolve, reject) => {
     //condition
-    let condition = 10;
+    let condition = 1;
 
     if (condition > 5) {
         resolve('Promise is resolved succesfully');
@@ -30,3 +30,9 @@ myPromise
         console.log(message);
     })
 
+/* El mensaje que recibe then or catch es el qu se establee en caso de resolvesrse
+    o rechazarse, es decir, si ocurre la condición, pasa el mensaje de resolve a
+    then, por eso veríamos 'Promise is resolved succesfully.
+    
+    De lo contrario, el message de reject pasa a catch, ya que no se cumplió la
+    promesa.*/
