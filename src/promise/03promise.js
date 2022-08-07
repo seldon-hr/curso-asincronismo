5376de53acf1121f62d94df96e43ee69b875fedc
Daniel Romero <daanmaro99june@gmail.com>
2022-08-06 22:34:53 -0500

    Promises

--- src/promise/03promise.js
+++ src/promise/03promise.js
@@ -7,6 +7,26 @@
 
 const myPromise = new Promise((resolve, reject) => {
     //condition
+    let condition = 10;
 
+    if (condition > 5) {
+        resolve('Promise is resolved succesfully');
+    } else {
+        reject('Promise is rejected');
+    }
 });
+
+/* Dos casos sobre lo que ocurrar con la promsea
+    Si es resuelta que va a ocurrir
+    de lo contrario es el otro paso.
+    .then para promesa resuelta
+    .catch para lo contrario
+*/
+
+myPromise
+    .then((message) => {
+        console.log(message); 
+    }).catch((message) => {
+        console.log(message);
+    })
 
