9cd595ee5e8887e344e08a7d8dd0ac0a122a3d07
Daniel Romero <daanmaro99june@gmail.com>
2022-08-06 22:28:12 -0500

    With promise

---
+++ src/promise/03promise.js
@@ -0,0 +1,12 @@
+// Usamos un constructor para crear un objeto Promise
+/* Añadimso dos parámetros éxito y rechazo,
+    funcioón como condition
+
+*/
+
+
+const myPromise = new Promise((resolve, reject) => {
+    //condition
+
+});
+
